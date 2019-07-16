c04c4bf1aa43c08f4052b821cd6ce6db11e71aae
aldo <aldo.perez@thinkskink.com>
2019-07-16 17:17:06 -0500

    validates if employee is ACTIVE for update and delete

--- src/main/java/minicode/employees/controller/EmployeeController.java
+++ src/main/java/minicode/employees/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package minicode.employees.controller;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -36,85 +35,93 @@ public class EmployeeController {
         this.repository = repository;
     }
 
-    @RequestMapping(value="/employees",method=RequestMethod.GET)
-    public List<Employee> getAll() {
-        logger.info("getting ACTIVE employees");
-                
-        return repository.findAll()
-        		.stream()	// Java 8: The Stream API. It helps to support functional style coding like filter, map and reduce. 
-        		.filter(employee -> Status.ACTIVE.toString().equals(employee.getStatus())) //Java 8: Lambda expressions, it let us usa an anonymous function as a method argument
-        		.collect(Collectors.toList());
+	@RequestMapping(value = "/employees", method = RequestMethod.GET)
+	public List<Employee> getAll() {
+		logger.info("getting ACTIVE employees");
+
+		return repository.findAll().stream() // Java 8: The Stream API. It helps to support functional style coding like
+												// filter, map and reduce.
+				.filter(employee -> Status.ACTIVE.toString().equals(employee.getStatus())) // Java 8: Lambda
+																							// expressions, it let us
+																							// usa an anonymous function
+																							// as a method argument
+				.collect(Collectors.toList());
 	}
 
-    @RequestMapping(value="/employees/{id}",method=RequestMethod.GET)
-    public Employee getEmployee(
-        @ApiParam("Employee Id number to fetch")
-        @Valid @Positive @PathVariable Long id) {
+	@RequestMapping(value = "/employees/{id}", method = RequestMethod.GET)
+	public Employee getEmployee(@ApiParam("Employee Id number to fetch") @Valid @Positive @PathVariable Long id) {
 
-        logger.info("getting employee with id:"+id);
+		logger.info("getting employee with id:" + id);
 
-        try{
-            return repository.findById(id)
-            .filter(employee -> Status.ACTIVE.toString().equals(employee.getStatus()))
-            .orElseThrow(()-> new EmployeeNotFoundException(id));    // Java 8: Optional<T>, this functionallity is a container that may or may not a non null, this class implements de method orElseThrow, which in case of null value will throw an exception of th type that we supplied
+		try {
+			return repository.findById(id).filter(employee -> Status.ACTIVE.toString().equals(employee.getStatus()))
+					.orElseThrow(() -> new EmployeeNotFoundException(id)); // Java 8: Optional<T>, this functionallity
+																			// is a container that may or may not a non
+																			// null, this class implements de method
+																			// orElseThrow, which in case of null value
+																			// will throw an exception of th type that
+																			// we supplied
 
-        }catch(EmployeeNotFoundException ex){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee Not Found");
-        }
-       
-    }
+		} catch (EmployeeNotFoundException ex) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee Not Found");
+		}
+
+	}
     
-    @RequestMapping(value="/employees",method=RequestMethod.POST)
-    public Employee createEmployee(
-    		@ApiParam("Info to create a new employee")
-    		@RequestBody @Valid EmployeeInput newEmployee) {
+	@RequestMapping(value = "/employees", method = RequestMethod.POST)
+	public Employee createEmployee(
+			@ApiParam("Info to create a new employee") @RequestBody @Valid EmployeeInput newEmployee) {
 
-    	logger.info("creating new employee");
-        
-        Employee employee = new Employee(
-        		null,newEmployee.getFirstName(),newEmployee.getMiddleInitial(),
-        		newEmployee.getLastName(),newEmployee.getDateOfBirth(),
-        		newEmployee.getDateOfEmployment(),Status.ACTIVE.toString());
-        
-        return repository.save(employee);
-    }
+		logger.info("creating new employee");
+
+		Employee employee = new Employee(null, newEmployee.getFirstName(), newEmployee.getMiddleInitial(),
+				newEmployee.getLastName(), newEmployee.getDateOfBirth(), newEmployee.getDateOfEmployment(),
+				Status.ACTIVE.toString());
+
+		return repository.save(employee);
+	}
     
-    @RequestMapping(value="/employees/{id}",method=RequestMethod.PUT)
-    public Optional<Employee> updateEmployee(
-    		@ApiParam("Employee info to update")
-    		@RequestBody @Valid UpdateEmployeeInput employeeToUpdate,
-    		@ApiParam("Employee Id number to update")
-   	     	@Valid @Positive @PathVariable Long id) {
+	@RequestMapping(value = "/employees/{id}", method = RequestMethod.PUT)
+	public Employee updateEmployee(
+			@ApiParam("Employee info to update") @RequestBody @Valid UpdateEmployeeInput employeeToUpdate,
+			@ApiParam("Employee Id number to update") @Valid @Positive @PathVariable Long id) {
 
-    	logger.info("updating employee with id:"+id);
+		logger.info("updating employee with id:" + id);
+		try {
+			return repository.findById(id).filter(employee -> Status.ACTIVE.toString().equals(employee.getStatus()))
+					.map(employee -> { // Java 8: Optional<T> map method, it applies the provides function, in this
+										// case for updating the employee, and returns an Optional<T> of the result
+						if (employeeToUpdate.getFirstName() != null)
+							employee.setFirstName(employeeToUpdate.getFirstName());
+						if (employeeToUpdate.getMiddleInitial() != null)
+							employee.setMiddleInitial(employeeToUpdate.getMiddleInitial());
+						if (employeeToUpdate.getLastName() != null)
+							employee.setLastName(employeeToUpdate.getLastName());
+						if (employeeToUpdate.getDateOfBirth() != null)
+							employee.setDateOfBirth(employeeToUpdate.getDateOfBirth());
+						if (employeeToUpdate.getDateOfEmployment() != null)
+							employee.setDateOfEmployment(employeeToUpdate.getDateOfEmployment());
+						return repository.save(employee);
+					}).orElseThrow(() -> new EmployeeNotFoundException(id));
+		} catch (EmployeeNotFoundException ex) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee Not Found");
+		}
+	}
 
-        return repository.findById(id)
-            .map(employee -> {  //Java 8: Optional<T> map method, it applies the provides function, in this case for updating the employee, and returns an Optional<T> of the result
-                if(employeeToUpdate.getFirstName()!=null)employee.setFirstName(employeeToUpdate.getFirstName());
-                if(employeeToUpdate.getMiddleInitial()!=null)employee.setMiddleInitial(employeeToUpdate.getMiddleInitial());
-                if(employeeToUpdate.getLastName()!=null)employee.setLastName(employeeToUpdate.getLastName());
-                if(employeeToUpdate.getDateOfBirth()!=null)employee.setDateOfBirth(employeeToUpdate.getDateOfBirth());
-                if(employeeToUpdate.getDateOfEmployment()!=null)employee.setDateOfEmployment(employeeToUpdate.getDateOfEmployment());
-                return repository.save(employee);
-            });
+	@RequestMapping(value = "/employees/{id}", method = RequestMethod.DELETE)
+	public Employee deactivateEmployee(
+			@ApiParam("DELETE operation needs Authorization credentials, for testing use YWRtaW46YWRtaW4=") @RequestHeader(value = "Authorization") String authorization,
+			@ApiParam("Employee Id number to deactivate") @Valid @Positive @PathVariable Long id) {
 
-    }
-    
-    @RequestMapping(value="/employees/{id}",method=RequestMethod.DELETE)
-    public Employee deactivateEmployee(
-    		@ApiParam("DELETE operation needs Authorization credentials, for testing use YWRtaW46YWRtaW4=")
-    		@RequestHeader(value="Authorization") String authorization,
-    		@ApiParam("Employee Id number to deactivate")
-    	    @Valid @Positive @PathVariable Long id) {
-
-        logger.info("deactivating employee with id:"+id);
-      try{  
-    	return repository.findById(id).map(employee -> {
-            employee.setStatus(Status.INACTIVE.toString());
-            return repository.save(employee);
-        }).orElseThrow(()-> new EmployeeNotFoundException(id));
-      }catch(EmployeeNotFoundException ex){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee Not Found");
-        }
-    }
+		logger.info("deactivating employee with id:" + id);
+		try {
+			return repository.findById(id).filter(employee -> Status.ACTIVE.toString().equals(employee.getStatus()))
+					.map(employee -> {
+						employee.setStatus(Status.INACTIVE.toString());
+						return repository.save(employee);
+					}).orElseThrow(() -> new EmployeeNotFoundException(id));
+		} catch (EmployeeNotFoundException ex) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee Not Found");
+		}
+	}
 }
